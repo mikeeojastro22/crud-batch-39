2d9dfb145b28694b1ada95d9472d41030e642f99
Mikee Ojastro <mikeeojastro@gmail.com>
2025-04-03 20:02:05 +0800

    April 3, 2025

--- src/App.js
+++ src/App.js
@@ -9,10 +9,23 @@ function App() {
   const [count, setCount] = useState(tasks.length);
 
   const handleTasks = (newTask) => {
+    // newTask = user input which we will get from the Task component
+    // updating the id
+    const newCount = count + 1;
+    setCount(newCount);
+
+    // [...prevtasks, newTask] - spread operator - creates a new array that contains the previous tasks and the newly created task
+    setTasks((prevtasks) => [...prevtasks, newTask]);
   }
 
   const handleDeleteTask = (taskId) => {
-
+    // filter - matches the condition then it will return the element if the condition is true
+    // setTasks - function binded to tasks
+    // prevtasks = tasks
+    // filter - creates a new array
+    // task - pertains to each element of tasks
+    // we want to exclude the task with taskId equal to taskId/deleting what the user wants to delete
+    setTasks((prevtasks) => prevtasks.filter((task) => task.id !== taskId));
   }
 
   return (
@@ -22,11 +35,13 @@ function App() {
           return (
             <div key={task.id}> 
               <span>{task.task_name}</span>
+              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
             </div>
           );
         })
       }
 
+      <Task handleTasks={handleTasks} newId={count} />
     </div>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,17 +1,33 @@
 import { useState } from "react";
 
 function Task(props){
+    const { handleTasks, newId } = props;
 
     // declaration for the new task 
     const [toDo, setToDo] = useState('');
 
     const addTaskHandler = (event) => {
+        // prevents the page from reloading
+        event.preventDefault();
 
+        const newTaskObject = {
+            task_name: toDo,
+            id: newId
+        }
+
+        handleTasks(newTaskObject);
+        setToDo('');
     }
 
     return (
         <div className="task">
-
+            <form onSubmit={addTaskHandler}>
+                <input 
+                    value={toDo} 
+                    placeholder="Add task here" 
+                    onChange={(event) => setToDo(event.target.value)}></input>
+                <button>Add Task</button>
+            </form>
         </div>
     );
 }
